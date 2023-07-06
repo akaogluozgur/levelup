cbbfb5598ca2d6067036568492361147ceba1f7f
akaogluozgur <akaogluozgur@gmail.com>
2023-07-07 00:29:06 +0300

    Adds initial implementation of backend & some more formatting

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Question, Post, Choice, Game, Tag, Author 
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email",)
+    
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ("tags",)
+    
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("game",)
+    list_display = ("title", "author", "rating", "date",) 
+    prepopulated_fields = {"slug": ("title",)}
+    
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Choice)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Tag)
+admin.site.register(Author, AuthorAdmin)
+
--- blog/views.py
+++ blog/views.py
@@ -1,61 +1,23 @@
-from django.shortcuts import render
-from datetime import date
-from copy import deepcopy
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "dota2",
-        "image": "dota2.jpg",
-        "author": "Özgür",
-        "date": date(2021, 7, 22),
-        "title": "Dota2 Review",
-        "excerpt": "Defence of the ancients review",
-        "content": """Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam quam fuga fugit, accusantium dolorum tempora animi 
-                magnam ad doloremque facilis voluptatibus natus? Numquam 
-                nisi reiciendis reprehenderit quam modi incidunt aut?
-                """,
-    },
-    {
-        "slug": "cyberpunk",
-        "image": "cyberpunk.jpg",
-        "author": "Özgür",
-        "date": date(2022, 6, 11),
-        "title": "Cyberpunk Review",
-        "excerpt": "Cyberpunk review",
-        "content": """Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam quam fuga fugit, accusantium dolorum tempora animi 
-                magnam ad doloremque facilis voluptatibus natus? Numquam 
-                nisi reiciendis reprehenderit quam modi incidunt aut?
-                """,
-    },
-    {
-        "slug": "darkest-dungeon-2",
-        "image": "darkest_dungeon2.jpg",
-        "author": "Özgür",
-        "date": date(2020, 7, 16),
-        "title": "Darkest Dungeon 2",
-        "excerpt": "Darkest Dungeon 2 is review",
-        "content": """Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nam quam fuga fugit, accusantium dolorum tempora animi 
-                magnam ad doloremque facilis voluptatibus natus? Numquam 
-                nisi reiciendis reprehenderit quam modi incidunt aut?
-                """,
-    }
-]
 # Create your views here.
 
 def index(request):
-    post_list = deepcopy(all_posts)
-    post_list.sort(key=lambda post: post.get("date"))
-    posts = post_list[-3:]
+    posts = Post.objects.all().order_by("date")[:3]
     return render(request, "blog/index.html", {"posts": posts})
 
 def posts(reqests):
-    post_list = deepcopy(all_posts)
-    post_list.sort(key=lambda post: post.get("date"))
-    return render(reqests, "blog/posts.html", {"posts": post_list})
+    posts = Post.objects.all().order_by("date")
+    return render(reqests, "blog/posts.html", {"posts": posts})
 
 def post_detail(request, slug):
-    selected_post = next(post for post in all_posts if post.get("slug") == slug)
-    return render(request, "blog/post-detail.html", {"post": selected_post})
+    selected_post = get_object_or_404(Post, slug=slug)
+    return render(
+        request,
+        "blog/post-detail.html",
+        {
+            "post": selected_post,
+            "post_tags": selected_post.game.tags.all()
+        }
+    )
